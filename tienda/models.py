ca7e8beb97e2991232025914060f89c48935d226
FrancoRegg <francoreggiardo97@gmail.com>
2024-10-21 17:35:47 +0200

    Añado los primeros modelos, categoria y producto

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+
+class Product(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE) 
