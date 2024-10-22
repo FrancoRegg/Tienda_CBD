6607524f6647dcabbdbd38025c552edc3782089f
FrancoRegg <francoreggiardo97@gmail.com>
2024-10-22 19:47:50 +0200

    Añado las primeras views basadas en clase

--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets
+from .models import Product, Category
+from .serializers import ProductoSerializer, CategoriaSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategoriaSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductoSerializer
