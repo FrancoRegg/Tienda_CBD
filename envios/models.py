d6bc8f284671bd873ec88d24ecddb06f3cb5ec3e
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-10 19:15:59 +0100

    Correccion ortografica

--- envios/models.py
+++ envios/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Shipping (models.Model):
-    order = models.ForeingKey('pedidos.Order', on_delete=models.CASCADE)
+    order = models.ForeignKey('pedidos.Order', on_delete=models.CASCADE)
     address = models.ForeignKey('usuarios.Address', on_delete=models.CASCADE)
     shipping_date = models.DateTimeField()
