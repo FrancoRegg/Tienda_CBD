06942818fa197fe2a4ee5fe8f7797bc09c33676b
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-10 19:03:38 +0100

    Modelo para la gestion de pagos

--- pagos/models.py
+++ pagos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    order = models.ForeignKey('pedidos.Order', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, default='Completed')
