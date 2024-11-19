ea7ba9ce62d70cc314fce37e047ef5706f2337a1
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-19 16:05:18 +0100

    Rutas para usuarios, funcionando

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('users/', get_users, name="get_users"),
     path('user/<int:pk>/', get_user_id, name="get_user_id"),
     path('user-create/', create_user, name="create_user"),
+    path('user-update/<int:pk>/', update_user, name="update_user"),
+    path('user-delete/<int:pk>/', delete_user, name="delete_user")
 ]
