496a17acd7798ad3ed9aa7a00d1d843f3630cdea
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-19 17:30:04 +0100

    Nueva ruta para obtener direcciones(Es de prueba)

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('user/<int:pk>/', get_user_id, name="get_user_id"),
     path('user-create/', create_user, name="create_user"),
     path('user-update/<int:pk>/', update_user, name="update_user"),
-    path('user-delete/<int:pk>/', delete_user, name="delete_user")
+    path('user-delete/<int:pk>/', delete_user, name="delete_user"),
+    path('address/', get_address, name="get_address"),
 ]
