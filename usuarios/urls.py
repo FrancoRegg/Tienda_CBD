500694132b3d818200bff43c41b7f63c351f9ee2
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-19 12:16:13 +0100

    Rutas creadas para mostrar usuarios, usuario por id y crear usuario. Todo funciona

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from usuarios.views import *
 
 urlpatterns = [
-    path('usuarios', usuarios)
+    path('users/', get_users, name="get_users"),
+    path('user/<int:pk>/', get_user_id, name="get_user_id"),
+    path('user-create/', create_user, name="create_user"),
 ]
