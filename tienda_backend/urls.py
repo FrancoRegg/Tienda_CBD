63fe950218cb83d25f59e58fc4a4a6132a0a0080
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-21 11:19:03 +0100

    Añado el template donde se va a renderizar la app

--- tienda_backend/urls.py
+++ tienda_backend/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path ,include
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -28,3 +29,5 @@ urlpatterns = [
     path('', include('apps.envios.urls')),
     path('', include('apps.pagos.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name="index.html"))]
