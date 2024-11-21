9f96029e60114ba6fdeb018ba918ddcf326475c9
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-21 17:50:49 +0100

    Añado los primeros componente a la pagina de inicio (Probando...)

--- src/containers/pages/home.jsx
+++ src/containers/pages/home.jsx
@@ -1,9 +1,14 @@
+import Footer from "components/navigation/Footer"
+import NavBar from "components/navigation/NavBar"
+
 function Home() {
     
     return(
-        <div>
+        <>
+           <NavBar/>
             HOME
-        </div>
+            <Footer/>
+        </>
     )
 }
 
