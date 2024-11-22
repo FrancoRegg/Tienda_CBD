f31814585898d3b8d5b02ec7c451565706d49448
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-22 13:11:45 +0100

    Incorporo las vistas al enrutamiento

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import store from "store";
 import { Provider } from "react-redux";
 
 import Error404 from "containers/errors/error404";
-import Home from "containers/pages/home";
-
+import Home from "containers/pages/HomeTemp";
+import Categoria1 from "containers/pages/Categoria1";
+import Categoria2 from "containers/pages/Categoria2";
+import Categoria3 from "containers/pages/Categoria3";
+import Categoria4 from "containers/pages/Categoria4";
+import Categoria5 from "containers/pages/categoria5";
 
 
 function App() {
@@ -15,12 +19,15 @@ function App() {
       <Router>
         <Routes>
           <Route path="*" element={<Error404 />} /> 
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home/>} />
+          <Route path="/categoria1" element={<Categoria1/>} /> 
+          <Route path="/categoria2" element={<Categoria2/>} /> 
+          <Route path="/categoria3" element={<Categoria3/>} /> 
+          <Route path="/categoria4" element={<Categoria4/>} /> 
+          <Route path="/categoria5" element={<Categoria5/>} />  
         </Routes>
       </Router>
-
     </Provider>
-    
   );
 
 }
