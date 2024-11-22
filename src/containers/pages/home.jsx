e5e470d6b7c585b05093cffcb7caf76bdf503626
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-22 08:57:02 +0100

    Incorporo Layout en mi home

--- src/containers/pages/home.jsx
+++ src/containers/pages/home.jsx
@@ -1,14 +1,15 @@
 import Footer from "components/navigation/Footer"
 import NavBar from "components/navigation/NavBar"
+import Layout from "hocs/layouts/Layout"
 
 function Home() {
     
     return(
-        <>
+        <Layout>
            <NavBar/>
             HOME
             <Footer/>
-        </>
+        </Layout>
     )
 }
 
