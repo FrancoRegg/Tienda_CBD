0d3a411fb16a418522ac3b6afd8c2c7720743780
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-22 12:07:54 +0100

    Estructuracion del navbar lista

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,10 +1,28 @@
 import { connect } from "react-redux"
-//import './../../styles/NavBar.css'
+import './../../styles/NavBar.css'
+import { Link } from "react-router-dom"
 
 function NavBar(){
     return(
-        <nav className="w-full bg-black">
-            navbar
+        <nav className="nav">
+            <div className="nav-container">
+            <div className="nav-container-items">
+                <div className="nav-logo">
+                    <Link to='/'>Logo de Marca</Link>
+                </div>
+                <div className="nav-container-categorias">
+                    <Link to=''className="categorias">Categoria 1</Link>
+                    <Link to=''className="categorias">Categoria 2</Link>
+                    <Link to=''className="categorias">Categoria 3</Link>
+                    <Link to=''className="categorias">Categoria 4</Link>
+                    <Link to=''className="categorias">Categoria 5</Link>
+                </div>
+                <div className="nav-container-usuario">
+                    <Link to=''><i class="bi bi-person-circle icon"></i></Link>
+                    <Link to=''><i class="bi bi-cart3 icon"></i></Link>
+                </div>
+            </div>
+            </div>
         </nav>
     )
 }
