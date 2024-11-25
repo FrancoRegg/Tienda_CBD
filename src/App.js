6cdd22a353ba96f65ddc6349043663269a41091e
FrancoRegg <francoreggiardo97@gmail.com>
2024-11-25 18:59:03 +0100

    Actualizacion de archivos y rutas

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import { Provider } from "react-redux";
 
 import Error404 from "containers/errors/error404";
 import Home from "containers/pages/HomeTemp";
-import Categoria1 from "containers/pages/Categoria1";
-import Categoria2 from "containers/pages/Categoria2";
-import Categoria3 from "containers/pages/Categoria3";
-import Categoria4 from "containers/pages/Categoria4";
-import Categoria5 from "containers/pages/categoria5";
-
+import FloresCbd from "containers/pages/Flores_cbd";
+import AceiteCbd from "containers/pages/Aceite_cbd";
+import VaperCbd from "containers/pages/Vaper_cbd";
+import PackCbd from "containers/pages/Packs_cbd";
+import FisioCbd from "containers/pages/Fisio_cbd";
 
 function App() {
   return (
@@ -20,11 +19,11 @@ function App() {
         <Routes>
           <Route path="*" element={<Error404 />} /> 
           <Route path="/" element={<Home/>} />
-          <Route path="/categoria1" element={<Categoria1/>} /> 
-          <Route path="/categoria2" element={<Categoria2/>} /> 
-          <Route path="/categoria3" element={<Categoria3/>} /> 
-          <Route path="/categoria4" element={<Categoria4/>} /> 
-          <Route path="/categoria5" element={<Categoria5/>} />  
+          <Route path="/flores-cbd" element={<FloresCbd/>} /> 
+          <Route path="/aceite-cbd" element={<AceiteCbd/>} /> 
+          <Route path="/vaper-cbd" element={<VaperCbd/>} /> 
+          <Route path="/pack-cbd" element={<PackCbd/>} /> 
+          <Route path="/fisioterapia-cbd" element={<FisioCbd/>} />  
         </Routes>
       </Router>
     </Provider>
